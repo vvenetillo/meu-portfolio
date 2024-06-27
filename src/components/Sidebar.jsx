ed20989b63d11cf4537fd02875ac43c104a3cb28
vvenetillo <venetillovictor@gmail.com>
2024-06-26 23:12:18 -0300

    update technologies and curriculo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -33,8 +33,8 @@ const Sidebar = () => {
       <SocialNetworks />
       <InformationContainer />
       <a
-        href="curriculo/JoseVictorDev.pdf"
-        download="JoseVictorDev.pdf"
+        href="curriculo/JVictorDev2024.pdf"
+        download="JVictorDev2024.pdf"
         className="btn"
       >
         Download currículo
